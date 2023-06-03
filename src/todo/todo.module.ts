7731f9f1ee16e3111624a17f8600ea3701948c3d
Shrestha Reewaj <r.waj555@gmail.com>
2023-06-03 13:25:37 +0545

    initial commit

---
+++ src/todo/todo.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
+@Module({
+  imports: [],
+  controllers: [TodoController],
+  providers: [TodoService],
+})
+export class AppModule {}
